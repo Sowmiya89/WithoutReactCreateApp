eed19c9ed9ceed3ddd6c1c85bcc26ef732bc886e
C <sowmiya.2.c@atos.net>
2020-06-09 23:10:36 +0530

    Timer issue resolved

--- src/Hooks.js
+++ src/Hooks.js
@@ -5,14 +5,16 @@ import Home from './Home.js';
     const [count, setCount] = useState(10);
 
     useEffect(() => {
-        setInterval(() => setCount(count + 1),3000);
+        const timerID = setInterval(() => setCount(count + 1),3000);
         console.warn("message",count);
+
+        return () => clearInterval(timerID);
+
       },[count]);
    
       return (
         <div className = 'app_name'>
             <h1> {props.name}</h1>
-           
             <Home count = {count}/>
         </div>
     );
